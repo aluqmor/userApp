35eff623d5fc3ce0331939b549dec4a0f42ac0be
Álvaro Luque <aluqmor651@ieszaidinvergeles.org>
2025-02-03 09:10:05 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,24 @@ use Illuminate\Support\Facades\Auth;
 //     return view('welcome');
 // });
 
+// Rutas de autenticación y verificación de correo electrónico
 Auth::routes(['verify' => true]);
 
+// Ruta de la página principal
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+
+// Rutas para diferentes roles de usuario
 Route::get('admin', [App\Http\Controllers\AdministratorController::class, 'index'])->name('admin.index');
 Route::get('super', [App\Http\Controllers\AdministratorController::class, 'indexSuper'])->name('super.index');
 Route::get('guest', [App\Http\Controllers\HomeController::class, 'guest'])->name('guest');
+
+// Ruta para la página de inicio del usuario autenticado
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+
+// Ruta para la página de verificación de correo electrónico
 Route::get('/verificado', [App\Http\Controllers\HomeController::class, 'verificado'])->middleware(['auth', 'verified'])->name('verificado');
+
+// Rutas relacionadas con el perfil del usuario
 Route::put('home/password', [App\Http\Controllers\ProfileController::class, 'password'])->name('home.password');
 Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'show'])->middleware('auth')->name('profile.show');
 Route::post('/profile/change-password', [App\Http\Controllers\ProfileController::class, 'changePassword'])->middleware('auth')->name('profile.changePassword');
@@ -22,6 +32,8 @@ Route::post('/profile/change-email', [App\Http\Controllers\ProfileController::cl
 Route::post('/profile/change-username', [App\Http\Controllers\ProfileController::class, 'changeUsername'])->middleware('auth')->name('profile.changeUsername');
 Route::delete('/profile/delete', [App\Http\Controllers\ProfileController::class, 'delete'])->middleware('auth')->name('profile.delete');
 Route::put('home/update', [App\Http\Controllers\ProfileController::class, 'update'])->name('home.update');
+
+// Rutas relacionadas con la administración de usuarios
 Route::get('/admin/users', [App\Http\Controllers\AdministratorController::class, 'index'])->middleware(['auth', 'admin'])->name('admin.users');
 Route::get('/admin/users/create', [App\Http\Controllers\AdministratorController::class, 'create'])->middleware(['auth', 'admin'])->name('admin.users.create');
 Route::post('/admin/users', [App\Http\Controllers\AdministratorController::class, 'store'])->middleware(['auth', 'admin'])->name('admin.users.store');
